c7d4302914790fdeb9b287076fe0a32e8dadb919
Geethika Yalamanchili <122038896+yalamaga@users.noreply.github.com>
2023-01-19 10:21:23 -0500

    data in table

--- packages/api/src/microservices/Assessment-Service.js
+++ packages/api/src/microservices/Assessment-Service.js
@@ -1,13 +1,21 @@
-const { Assessment } = require(`../database/models`);
+import { Assessment } from '../database/models';
 
 exports.submit = async (assessment) => {
   // use the sequelize model Assessments from packages/api/src/database/models to save
-  // the assessment data in the PostgreSQL database
+  // the assessment data in the PostgreSQL databases
+  await Assessment.create({
+    catDateOfBirth: assessment.date,
+    catName: assessment.name,
+    instrumentType: 1,
+    riskLevel: assessment.riskLevel,
+    score: assessment.points,
+  });
 };
 
 exports.getList = () => {
   // use the sequelize model Assessments from packages/api/src/database/models to fetch
   // the assessment data from the PostgreSQL database
+  // testing pull request and merging
   const assessments = [];
 
   return assessments;
