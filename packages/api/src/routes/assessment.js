d27788575374f55e99a91d6d4804b2bbf6788e0e
Geethika Yalamanchili <122038896+yalamaga@users.noreply.github.com>
2023-01-19 10:21:35 -0500

    assessment

--- packages/api/src/routes/assessment.js
+++ packages/api/src/routes/assessment.js
@@ -6,10 +6,15 @@ const { Router } = require(`express`);
 const assessmentRouter = Router();
 
 assessmentRouter.post(
-  `/`,
+  `/submit`,
   async (req, res, next) => {
     try {
       const { assessment } = req.body;
+      // eslint-disable-next-line no-console
+      console.log(assessment);
+      const val = await AssessmentService.submit(assessment);
+      // eslint-disable-next-line no-console
+      console.log(val);
 
       // verify that your data is making it here to the API by using console.log(assessment);
       // call the AssessmentService.submit function from packages/api/src/microservices/Assessment-Service.js and
@@ -18,7 +23,7 @@ assessmentRouter.post(
       ResponseHandler(
         res,
         `Submitted assessment`,
-        {},
+        { assessment },
       );
     } catch (err) {
       next(err);
@@ -28,6 +33,7 @@ assessmentRouter.post(
 
 assessmentRouter.get(
   `/`,
+  // eslint-disable-next-line require-await
   async (req, res, next) => {
     try {
       // verify that your data is making it here to the API by using console.log();
