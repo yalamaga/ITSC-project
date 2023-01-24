662416a68c41a08d23229b0c011c42be98991858
Geethika Yalamanchili <122038896+yalamaga@users.noreply.github.com>
2023-01-24 10:22:31 -0500

    table

--- packages/client/src/pages/Assessments/AssessmentList.jsx
+++ packages/client/src/pages/Assessments/AssessmentList.jsx
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useMemo, useTable } from 'react-table';
+import { useMemo, useSortBy, useTable } from 'react-table';
 import { AssessmentService } from '../../services/AssessmentService';
 export const AssessmentList = () => {
   const [ assessments, setAssessments ] = useState([]);
-
   // fetch all assessments using the AssessmentService.getList function from OCAT/client/services/AssessmentService.js
-  useEffect(() => {
-    const fetchAssessments = async () => {
-      setAssessments(await AssessmentService.getList(`/client/services/AssessmentService.js`));
-    };
-    fetchAssessments(assessments);
-  }, [ assessments ]);
   const columns = useMemo(
     () => [
       {
@@ -18,52 +11,78 @@ export const AssessmentList = () => {
         columns: [
           {
             Header: `ID`,
-            accessor: `assessment.id`,
+            accessor: `id`,
           },
           {
             Header: `Cat Name`,
-            accessor: `assessment.name`,
+            accessor: `catName`,
           },
           {
             Header: `Cat Date of birth`,
-            accessor: `assessment.date`,
+            accessor: `catDateOfBirth`,
           },
           {
             Header: `Score`,
-            accessor: `assessment.points`,
+            accessor: `score`,
           },
           {
             Header: `Risk level`,
-            accessor: `assessment.riskLevel`,
+            accessor: `riskLevel`,
           },
         ],
       },
     ]
   );
-
+  useEffect(() => {
+    const fetchAssessments = async () => {
+      setAssessments(await AssessmentService.getList());
+    };
+    fetchAssessments();
+  }, [ ]);
+  const {
+    getTableBodyProps,
+    getTableProps,
+    headerGroups,
+    prepareRow,
+    rows,
+  } = useTable({ columns, data: assessments }, useSortBy);
   return (
     <div>
-      {
-        <table>
-          <thread>
-            <tr>
-              <th>Cat Name</th>
-              <th>Cat Date of Birth</th>
-              <th>Assessment score</th>
-              <th>Risk Level</th>
-            </tr>
-          </thread>
-          <tbody>
-            <tr>
-              <td>Abby</td>
-              <td>03-01-2019</td>
-              <td>4</td>
-              <td>high</td>
-            </tr>
-          </tbody>
-        </table>
-      }
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup =>
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column =>
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  {column.render(`Header`)}
+                  <span>
+                    {column.isSorted ?
+                      column.isSortedDesc ?
+                        `🔽` :
+                        `🔼` :
+                      ``}
+                  </span>
+                </th>)}
+            </tr>)}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell =>
+                  <td
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render(`Cell`)}
+                  </td>)}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-  // hgfdgfxcjhkb
 };
